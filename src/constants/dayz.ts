620165f28797674356645597e0a14cf173604afd
enzom-uy <enzom.dev@gmail.com>
2025-08-25 16:03:39 -0300

    feature: spit out random dayz facts

---
+++ src/constants/dayz.ts
@@ -0,0 +1,6 @@
+export const DAYZ_MESSAGES = [
+  `La interacción con los demás jugadores y la construcción hacia el siguiente encuentro.\nTanto vos como la otra persona seguramente estuvieron la última hora y media looteando, los dos tienen mucho que perder. Esto hace que los encuentros no se sientan como pvps fugaces en un call of duty sino que sea de vida o muerte literal.`,
+  `Los controles intuitivos hacen que se sienta como si estuvieras vos mismo controlando al personaje con tu propio cuerpo.`,
+  `Las diferentes reacciones de los jugadores al encontrarse con otra persona simula las reacciones que tendrían en un apocalipsis real.`,
+  `Uno de los juegos más visualmente impresionantes e increiblemente optimizados de la historia.`,
+];
