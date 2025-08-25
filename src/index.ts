8167218a9fd05e7ef4d4a2dc813e1537e17e5aa1
enzom-uy <enzom.dev@gmail.com>
2025-08-25 16:34:53 -0300

    bot status config
    bot status config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { Client, IntentsBitField, Partials } from "discord.js";
 import eventHandler from "./handlers/eventHandler";
+import { setupBot } from "./bot-config/config";
 
 dotenv.config();
 
@@ -24,6 +25,8 @@ export const client: Client = new Client({
   try {
     eventHandler(client);
     await client.login(process.env.DEV_TOKEN!);
+    console.log("Bot is ready!");
+    setupBot();
   } catch (error) {
     console.error(`Error: ${error}`);
   }
